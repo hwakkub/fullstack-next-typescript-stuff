9354be3d53d78d0374ae62c749db821a066392c8
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-11 11:45:27 +0700

    build_app & page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+ 
+export default function About() {
+  return (
+   <main>
+    <h1>About page</h1>
+   </main>
+  )
+}
